2ecd28718041d8b5cfff59b19299000515515326
Gilles Barbier <gilles@zenaton.com>
2020-02-02 14:17:19 +0100

    Title update

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,9 @@
 <template>
   <nav class="flex items-center justify-between flex-wrap bg-blue-500 p-6">
     <div class="flex items-center flex-shrink-0 text-white mr-6">
-      <span class="font-semibold text-xl tracking-tight">WebApp</span>
+      <span class="font-semibold text-xl tracking-tight"
+        >Example Of Web App Using Zenaton
+      </span>
     </div>
   </nav>
 </template>
